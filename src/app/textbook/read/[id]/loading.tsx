87085cd3a6b99a59e2fa66c903a6d3509de9073d
Nyein Kaung Swe <nyeinkaungswe@gmail.com>
2026-07-15 18:38:22 +0630

    Show book preview during reader route loading

--- src/app/textbook/read/[id]/loading.tsx
+++ src/app/textbook/read/[id]/loading.tsx
@@ -1,5 +1,5 @@
 import BookReaderLoading from '@/components/books/BookReaderLoading'
 
 export default function LoadingTextbookReader() {
-  return <BookReaderLoading message="Opening your book…" />
+  return <BookReaderLoading message="Opening book…" />
 }
